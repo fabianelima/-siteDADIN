b5ddca494b1774d47fcee9748dc0b2f15f3a4f25
fabianelima <fabianelim@gmail.com>
2017-07-24 14:48:28 -0300

    Mais modificações

--- template/head.php
+++ template/head.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,400i,600,600i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
@@ -21,6 +22,5 @@
   </head>
   <body>
     <div class="jumbotron text-left">
-      <h1>Departamento Acadêmico de Desenho Industrial</h1>
-      <p>Universidade Tecnológica Federal do Paraná</p>
+      <img src="img/logo.svg" width="35%">
     </div>
--- template/menu2.php
+++ template/menu2.php
@@ -1,11 +1,10 @@
 <div class="col-md-3 dadin-menu dadin-menu-2">
   <ul>
-    <li><span class="glyphicon glyphicon-search"></span> Busca</li>
-    <li><span class="glyphicon glyphicon-log-in"></span> Login</li>
-    <li><span class="glyphicon glyphicon-inbox"></span> Webmail</li>
-    <li><span class="glyphicon glyphicon-cloud-download"></span> Nuvem</li>
-    <li><span class="glyphicon glyphicon-user"></span> Contato</li>
-    <li><span class="glyphicon glyphicon-calendar"></span> Calendário</li>
-    <li><span class="glyphicon glyphicon-bullhorn"></span> Eventos</li>
+    <li><span class="glyphicon glyphicon-search"></span></li>
+    <li>login</li>
+    <li>webmail</li>
+    <li>nuvem</li>
+    <li>contato</li>
   </ul>
+  <img src="img/agenda.svg">
 </div>
